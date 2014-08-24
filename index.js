a324a9f535d37c6d006d01eafabcfd6774d7faf7
Harry Moreno <morenoh149@gmail.com>
2014-08-23 23:14:49 -0700

    finished version 1

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/'))
 
 app.get('/', function(request, response) {
-  response.send('Hello world');
+  response.sendfile('index.html');
 })
 
 app.listen(app.get('port'), function() {
