efb7a2b2d3cfd726bcdf8296a5b5316279f8fe7e
Harry Moreno <morenoh149@gmail.com>
2014-08-24 01:37:13 -0700

    made purecss custom build with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var concat = require('gulp-concat');
 
 gulp.task('default', function() {
   gulp.src('styles/*.sass')
     .pipe(sass())
     .pipe(gulp.dest('styles'));
+
+  gulp.src(['./bower_components/pure/pure.css', './bower_components/pure/base.css',
+  './bower_components/pure/grids-responsive.css', './bower_components/pure/grids.css'])
+    .pipe(concat('pure-bundle.css'))
+    .pipe(gulp.dest('./styles/'));
 });
