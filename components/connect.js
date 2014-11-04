f3a55b9dbbbe19c87de496db3b00b25f88c42522
Berkeley Martinez <Berkeley@robotie.com>
2014-11-03 20:09:16 -0800

    Adds input form for email. Still needs functionality.

--- components/connect.js
+++ components/connect.js
@@ -9,12 +9,23 @@ var Connect = React.createClass({
         <div className = 'connect_heading'>
           <h2>CONN<span className = 'threes'>3</span>CT</h2>
         </div>
-        <div>
-          <input
-            type = 'text'/>
-        </div>
-        <div>
-          <button className = 'pure-button'>Connect</button>
+
+        <div className = 'connect_email'>
+          <div>
+            <form className = 'pure-form'>
+              <div>
+                <input
+                  type = 'email'
+                  className = 'connect_input'
+                  placeholder = 'email'/>
+              </div>
+              <div>
+                <span>
+                  Connect
+                </span>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
