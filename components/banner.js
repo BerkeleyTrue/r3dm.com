2c45d3fdb18a801268f61f5f726b741b1fb7bd93
Berkeley Martinez <Berkeley@robotie.com>
2014-11-07 20:03:02 -0800

    Sets low res image as banner. Faster loading.

--- components/banner.js
+++ components/banner.js
@@ -1,34 +1,13 @@
 /** @jsx React.DOM */
 'use strict';
-var React = require('react'),
-    Image = require('react-imageloader');
+var React = require('react');
 
 var Banner = React.createClass({
+
   render: function() {
-    var banners = [
-      //'images/banner-l.jpg 1000w',
-      'images/banner-m.jpg 700w',
-      'images/banner-s.jpg 500w'
-    ].join(', ');
-
-    var sizes = [
-      '(max-width: ) 100vw',
-      '(max-width: ) 100vw',
-      '(max-width: ) 100vw',
-      '(max-width: ) 100vw',
-    ].join(', ');
-
-    var preLoader = function() {
-      return (
-        <img srcSet = { banners } sizes = " "/>
-      );
-    };
-
     return (
       <div className = 'first-con_top'>
-        <Image
-          src = 'images/banner-xl.jpg'
-          preloader = { preLoader }/>
+        <img src = 'images/banner.jpg'/>
       </div>
     );
   }
