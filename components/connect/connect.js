6c09e70b5f5b9898b90fd2ef6ab869c17953ef05
Harry Moreno <morenoh149@gmail.com>
2014-12-31 15:03:30 -0800

    removed console log. Need a couple of weeks to confirm GA is tracking this event

--- components/connect/connect.js
+++ components/connect/connect.js
@@ -37,7 +37,6 @@ var Connect = React.createClass({displayName: "Connect",
 
     // submit event to Google Analytics to measure conversion goals
     globular.ga('send', 'event', 'button', 'click', 'Connect');
-    console.log('ga is', globular.ga);
   },
 
   render: function() {
@@ -45,40 +44,40 @@ var Connect = React.createClass({displayName: "Connect",
         name = this.state.name;
 
     return (
-      React.createElement("div", {id: "connect", className: "connect"},
-        React.createElement("div", {className: "connect_heading"},
+      React.createElement("div", {id: "connect", className: "connect"}, 
+        React.createElement("div", {className: "connect_heading"}, 
           React.createElement("h2", null, "Work With Us.")
-        ),
+        ), 
 
-        React.createElement("div", {className: "connect_form"},
-          React.createElement("div", null,
+        React.createElement("div", {className: "connect_form"}, 
+          React.createElement("div", null, 
             React.createElement("form", {
-              action: "",
-              className: "pure-form",
-              onSubmit:  this.handleConnect},
-              React.createElement("div", {className: "connect_name"},
+              action: "", 
+              className: "pure-form", 
+              onSubmit:  this.handleConnect}, 
+              React.createElement("div", {className: "connect_name"}, 
                   React.createElement("input", {
-                    type: "text",
-                    name: "name",
-                    className: "connect_input",
-                    value: name,
-                    onChange:  this.onNameChange,
+                    type: "text", 
+                    name: "name", 
+                    className: "connect_input", 
+                    value: name, 
+                    onChange:  this.onNameChange, 
                     placeholder: "your name"})
-              ),
-              React.createElement("div", {className: "connect_email"},
-                React.createElement("div", null,
+              ), 
+              React.createElement("div", {className: "connect_email"}, 
+                React.createElement("div", null, 
                   React.createElement("input", {
-                    type: "email",
-                    name: "email",
-                    className: "connect_input",
-                    value: email,
-                    onChange:  this.onEmailChange,
+                    type: "email", 
+                    name: "email", 
+                    className: "connect_input", 
+                    value: email, 
+                    onChange:  this.onEmailChange, 
                     placeholder: "email"})
-                ),
+                ), 
                 React.createElement("div", {
-                  className: "button",
-                  onClick:  this.handleConnect},
-                  React.createElement("span", null,
+                  className: "button", 
+                  onClick:  this.handleConnect}, 
+                  React.createElement("span", null, 
                     "Connect"
                   )
                 )
