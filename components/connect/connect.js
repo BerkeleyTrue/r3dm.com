e55c29a6cb4128be37abffed2e68c863f1422b7d
Harry Moreno <morenoh149@gmail.com>
2014-12-31 13:47:14 -0800

    removed trailing whitespace

--- components/connect/connect.js
+++ components/connect/connect.js
@@ -44,40 +44,40 @@ var Connect = React.createClass({displayName: "Connect",
         name = this.state.name;
 
     return (
-      React.createElement("div", {id: "connect", className: "connect"}, 
-        React.createElement("div", {className: "connect_heading"}, 
+      React.createElement("div", {id: "connect", className: "connect"},
+        React.createElement("div", {className: "connect_heading"},
           React.createElement("h2", null, "Work With Us.")
-        ), 
+        ),
 
-        React.createElement("div", {className: "connect_form"}, 
-          React.createElement("div", null, 
+        React.createElement("div", {className: "connect_form"},
+          React.createElement("div", null,
             React.createElement("form", {
-              action: "", 
-              className: "pure-form", 
-              onSubmit:  this.handleConnect}, 
-              React.createElement("div", {className: "connect_name"}, 
+              action: "",
+              className: "pure-form",
+              onSubmit:  this.handleConnect},
+              React.createElement("div", {className: "connect_name"},
                   React.createElement("input", {
-                    type: "text", 
-                    name: "name", 
-                    className: "connect_input", 
-                    value: name, 
-                    onChange:  this.onNameChange, 
+                    type: "text",
+                    name: "name",
+                    className: "connect_input",
+                    value: name,
+                    onChange:  this.onNameChange,
                     placeholder: "your name"})
-              ), 
-              React.createElement("div", {className: "connect_email"}, 
-                React.createElement("div", null, 
+              ),
+              React.createElement("div", {className: "connect_email"},
+                React.createElement("div", null,
                   React.createElement("input", {
-                    type: "email", 
-                    name: "email", 
-                    className: "connect_input", 
-                    value: email, 
-                    onChange:  this.onEmailChange, 
+                    type: "email",
+                    name: "email",
+                    className: "connect_input",
+                    value: email,
+                    onChange:  this.onEmailChange,
                     placeholder: "email"})
-                ), 
+                ),
                 React.createElement("div", {
-                  className: "button", 
-                  onClick:  this.handleConnect}, 
-                  React.createElement("span", null, 
+                  className: "button",
+                  onClick:  this.handleConnect},
+                  React.createElement("span", null,
                     "Connect"
                   )
                 )
