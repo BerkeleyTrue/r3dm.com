c522ca5ba634edba6bf7ce6ea36f8fed2ad6cc26
Harry Moreno <morenoh149@gmail.com>
2015-01-01 23:43:17 -0800

    added wintersmith for blogging

--- controllers/api.js
+++
@@ -1,8 +0,0 @@
-var Post = require('../models/post.js');
-
-exports.makeNewPost = function(req, res) {
-  new Post({title: 'test', author: 'berks'}).save();
-};
-
-exports.getPosts = function(req, res) {
-  Post.find(function
--- server.js
+++ server.js
@@ -18,9 +18,6 @@ var express = require('express'),
     Fetcher = require('fetchr'),
     mandrillServ = require('./services/mandrill'),
 
-    // ## database
-    mongoose = require('mongoose'),
-
     // ## Express/Serve
     morgan = require('morgan'),
     serve = require('serve-static'),
@@ -30,9 +27,6 @@ var express = require('express'),
     cookieParse = require('cookie-parser'),
     helmet = require('helmet');
     //coookieSess = require('cookie-session')
-
-// connect to database
-mongoose.connect(process.env.MONGO_URL);
 
 // ## State becomes a variable available to all rendered views
 state.extend(app);
@@ -52,6 +46,7 @@ Fetcher.registerFetcher(mandrillServ);
 app.use('/api', Fetcher.middleware());
 
 app.use(serve('./public'));
+app.use('/blog', serve('./blog/build'));
 
 app.get('/500', function(req, res) {
   res.render('500');
@@ -73,9 +68,6 @@ app.get('/emails/:name', function(req, res) {
   locals.name = nameArr[0];
   res.render('email/greet', locals);
 });
-
-app.get('/blog', api.blog);
-app.get('/blog/:title.:format?', api.post);
 
 app.get('/*', function(req, res, next) {
   Router.run(routes, req.path, function(Handler, state) {
