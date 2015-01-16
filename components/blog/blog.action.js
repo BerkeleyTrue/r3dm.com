a3bf1c99e1bdfa37d449454f35ee534145756578
Harry Moreno <morenoh149@gmail.com>
2015-01-16 10:57:29 -0800

    blog action outline

---
+++ components/blog/blog.action.js
@@ -0,0 +1,27 @@
+'use strict';
+var Rx = require('rx'),
+    rxAction = require('rx-flux').Action,
+    Fetcher = require('fetchr'),
+    debug = require('debug')('r3dm:connect:createConnect');
+
+var fetcher = new Fetcher({
+  xhrPath: '/api'
+});
+
+var action = rxAction.create();
+var complete = new Rx.Subject();
+
+action.subscribe(function(payload) {
+  debug('Creating email for: ', payload);
+  fetcher.create('mandrillService', payload, {}, {}, function(err, data) {
+    if (err) {
+      return complete.onError(err);
+    }
+    complete.onNext(data);
+  });
+});
+
+module.exports = {
+  action: action,
+  complete: complete
+};
