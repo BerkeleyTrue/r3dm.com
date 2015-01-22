8c1086903c12de63097289d88f8d05d2d3dd3c61
Harry Moreno <morenoh149@gmail.com>
2015-01-22 15:18:56 -0800

    rendering blog briefings-list. And can now click through to see the entire blog post

--- components/blog/Blog.js
+++ components/blog/Blog.js
@@ -1,17 +1,19 @@
 var React = require('react'),
-    Link = require('react-router').Link,
+    Router = require('react-router'),
+    Link = Router.Link,
     StateStreamMixin = require('rx-react').StateStreamMixin,
     BlogStore = require('./blog.store.js'),
     debug = require('debug')('r3dm:components:blog');
 
 var Blog = React.createClass({displayName: "Blog",
-  mixins: [StateStreamMixin],
+  mixins: [StateStreamMixin,
+           Router.State],
 
-  getStateStream: function() {
+  getStateStream: function () {
     return BlogStore;
   },
 
-  componentWillMount: function() {
+  componentWillMount: function () {
     debug('comp will mount');
     if (this.props.context) {
       debug('found context');
@@ -19,24 +21,35 @@ var Blog = React.createClass({displayName: "Blog",
     }
   },
 
-  render: function() {
+  render: function () {
     var posts = this.state.posts;
     if (!Array.isArray(posts)) {
       posts = [posts];
     }
     debug('posts', posts);
-    var val = posts.map(function(post) {
+
+    var val = posts.map(function (post) {
+      var html = posts.length === 1 ? post.content.extended :
+                                      post.content.brief;
+      debug('map:html', html);
       return (
-        React.createElement(Link, {to: "blog", params: { title: post.title}, key:  post.title}, 
-          React.createElement("div", {className: "post"}, 
-            React.createElement("h3", null,  post.title), 
-            React.createElement("span", {dangerouslySetInnerHTML: { __html: post.content.brief}})
-          )
+        React.createElement("div", {className: "post"}, 
+          React.createElement(Link, {to: "blog", params: { title: post.title}, key:  post.title, className: "post-title"}, 
+            React.createElement("h1", null,  post.title)
+          ), 
+          React.createElement("span", {dangerouslySetInnerHTML: { __html: html}})
         )
       );
     });
+
     debug('will render');
-    return React.createElement("div", null, val );
+    return (
+      React.createElement("div", {className: "pure-g"}, 
+        React.createElement("div", {className: "pure-u-1-6"}), 
+        React.createElement("div", {className: "posts-wrapper pure-u-2-3"}, val ), 
+        React.createElement("div", {className: "pure-u-1-6"})
+      )
+    );
   }
 });
 
--- components/blog/Blog.jsx
+++ components/blog/Blog.jsx
@@ -1,17 +1,19 @@
 var React = require('react'),
-    Link = require('react-router').Link,
+    Router = require('react-router'),
+    Link = Router.Link,
     StateStreamMixin = require('rx-react').StateStreamMixin,
     BlogStore = require('./blog.store.js'),
     debug = require('debug')('r3dm:components:blog');
 
 var Blog = React.createClass({
-  mixins: [StateStreamMixin],
+  mixins: [StateStreamMixin,
+           Router.State],
 
-  getStateStream: function() {
+  getStateStream: function () {
     return BlogStore;
   },
 
-  componentWillMount: function() {
+  componentWillMount: function () {
     debug('comp will mount');
     if (this.props.context) {
       debug('found context');
@@ -19,24 +21,35 @@ var Blog = React.createClass({
     }
   },
 
-  render: function() {
+  render: function () {
     var posts = this.state.posts;
     if (!Array.isArray(posts)) {
       posts = [posts];
     }
     debug('posts', posts);
-    var val = posts.map(function(post) {
+
+    var val = posts.map(function (post) {
+      var html = posts.length === 1 ? post.content.extended :
+                                      post.content.brief;
+      debug('map:html', html);
       return (
-        <Link to='blog' params={{ title: post.title }} key={ post.title}>
-          <div className='post'>
-            <h3>{ post.title }</h3>
-            <span dangerouslySetInnerHTML={{ __html: post.content.brief }} />
-          </div>
-        </Link>
+        <div className='post'>
+          <Link to='blog' params={{ title: post.title }} key={ post.title } className='post-title'>
+            <h1>{ post.title }</h1>
+          </Link>
+          <span dangerouslySetInnerHTML={{ __html: html }} />
+        </div>
       );
     });
+
     debug('will render');
-    return <div>{ val }</div>;
+    return (
+      <div className='pure-g'>
+        <div className='pure-u-1-6'></div>
+        <div className='posts-wrapper pure-u-2-3'>{ val }</div>
+        <div className='pure-u-1-6'></div>
+      </div>
+    );
   }
 });
 
