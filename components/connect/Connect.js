af53ec4ff98db3718ae3288e2354c3e58f630b8f
Berkeley Martinez <Berkeley@robotie.com>
2015-01-25 21:41:13 -0800

    Connect transitions working. Loading has spinner On success a thank you appears Error still needs styling. I want to change transitions to fade in.

--- components/connect/Connect.js
+++ components/connect/Connect.js
@@ -1,6 +1,7 @@
-var React = require('react'),
-    CSSTransitionGroup = React.addons.CSSTransitionGroup,
-    { StateStreamMixin } = require('rx-react'),
+var Rx = require('rx'),
+    React = require('react'),
+
+  { StateStreamMixin } = require('rx-react'),
 
     globular = require('../globular'),
     debug = require('debug')('r3dm:components:connect'),
@@ -8,18 +9,55 @@ var React = require('react'),
     ConnectActions = require('./Actions'),
     ConnectStore = require('./Store');
 
+var {
+    CSSTransitionGroup,
+    PureRenderMixin
+  } = React.addons;
+
+var Sent = React.createClass({displayName: "Sent",
+
+  mixins: [PureRenderMixin],
+
+  render: function() {
+    return (
+      React.createElement("div", {
+        style:  this.props.style, 
+        className:  this.props.className, 
+        key: "sent"}, 
+        React.createElement("h1", null, "Thanks!"), 
+        React.createElement("p", null, "You should see an email from us soon.")
+      )
+    );
+  }
+});
+
 var Connect = React.createClass({displayName: "Connect",
 
   mixins: [StateStreamMixin],
 
-  _onEmailChange: ConnectActions.onEmailChange,
-  _onNameChange: ConnectActions.onNameChange,
-
   getStateStream: function() {
     debug('setting up state stream');
-    return ConnectStore;
+    return Rx.Observable.combineLatest(
+      ConnectStore,
+      this._compHeightAction,
+      function(state, height) {
+        state.height = height;
+        return state;
+      }
+    );
   },
 
+  componentDidMount: function() {
+    var connectForm = this.refs.form.getDOMNode();
+
+    this._compHeightAction.onNext(connectForm.clientHeight);
+  },
+
+  _onEmailChange: ConnectActions.onEmailChange,
+  _onNameChange: ConnectActions.onNameChange,
+  _compHeightAction: new Rx.BehaviorSubject(false),
+
+  // TODO: turn this to a single action
   _handleConnect: function(e) {
     var email = this.state.email,
         name = this.state.name;
@@ -46,70 +84,121 @@ var Connect = React.createClass({displayName: "Connect",
       name,
       sending,
       sent,
-      error
+      error,
+      height
     } = this.state;
-    var view = [];
-    if (!sending && !error && !sent) {
-      view.push(
-        React.createElement("div", {id: "connect", className: "connect", key: "init"}, 
-          React.createElement("div", {className: "connect_heading"}, 
-            React.createElement("h2", null, "Work With Us.")
-          ), 
-          React.createElement("div", {className: "connect_form"}, 
-            React.createElement("div", null, 
-              React.createElement("form", {
-                action: "", 
-                className: "pure-form", 
-                onSubmit:  this.handleConnect}, 
-                React.createElement("div", {className: "connect_name"}, 
-                    React.createElement("input", {
-                      type: "text", 
-                      name: "name", 
-                      className: "connect_input", 
-                      value: name, 
-                      onChange:  this._onNameChange, 
-                      placeholder: "your name"})
-                ), 
-                React.createElement("div", {className: "connect_email"}, 
-                  React.createElement("div", null, 
-                    React.createElement("input", {
-                      type: "email", 
-                      name: "email", 
-                      className: "connect_input", 
-                      value: email, 
-                      onChange:  this._onEmailChange, 
-                      placeholder: "email"})
-                  ), 
-                  React.createElement("div", {
-                    className: "button", 
-                    onClick:  this._handleConnect}, 
-                    React.createElement("span", null, 
-                      "Connect"
-                    )
-                  )
+
+    var style = {
+      height: height + 'px'
+    };
+
+    var sendingView = (
+      React.createElement("div", {
+        ref: "sending", 
+        style: style, 
+        className: "connect connect_sending", 
+        key: "sending"}, 
+        React.createElement("svg", {
+          className: "connect_sending-spinner", 
+          width: "65px", 
+          height: "65px", 
+          viewBox: "0 0 66 66", 
+          xmlns: "http://www.w3.org/2000/svg"}, 
+          React.createElement("circle", {
+            className: "connect_sending-path", 
+            fill: "none", 
+            strokeWidth: "6", 
+            strokeLinecap: "round", 
+            cx: "33", 
+            cy: "33", 
+            r: "30"}
+          )
+        )
+      )
+    );
+    var sentView = (
+      React.createElement(Sent, {
+        ref: "sent", 
+        className: "connect", 
+        style: style })
+    );
+    var errorView = (
+      React.createElement("span", {
+        style: style, 
+        className: "connect connect-error", 
+        key: "error"}, "Error")
+    );
+    var formView = (
+      React.createElement("div", {
+        className: "connect", 
+        key: "init", 
+        ref: "form"}, 
+        React.createElement("div", {className: "connect_heading"}, 
+          React.createElement("h2", null, "Work With Us.")
+        ), 
+        React.createElement("div", {className: "connect_form"}, 
+          React.createElement("form", {
+            action: "", 
+            className: "pure-form", 
+            onSubmit:  this.handleConnect}, 
+            React.createElement("div", {className: "connect_name"}, 
+                React.createElement("input", {
+                  type: "text", 
+                  name: "name", 
+                  className: "connect_input", 
+                  value: name, 
+                  onChange:  this._onNameChange, 
+                  placeholder: "your name"})
+            ), 
+            React.createElement("div", {className: "connect_email"}, 
+              React.createElement("div", null, 
+                React.createElement("input", {
+                  type: "email", 
+                  name: "email", 
+                  className: "connect_input", 
+                  value: email, 
+                  onChange:  this._onEmailChange, 
+                  placeholder: "email"})
+              ), 
+              React.createElement("div", {
+                className: "button", 
+                onClick:  this._handleConnect}, 
+                React.createElement("span", null, 
+                  "Connect"
                 )
               )
             )
           )
         )
-      );
-    } else if (sending) {
-      view.push(
-        React.createElement("div", {key: "sending"})
-      );
-    } else if (sent) {
-      view.push(
-        React.createElement("span", {key: "sent"}, "Email sent")
-      );
-    } else {
-      view.push(
-        React.createElement("span", {key: "error"}, "Error")
-      );
-    }
+      )
+    );
 
     return (
-      React.createElement(CSSTransitionGroup, {transitionName: "connect"}, 
-        view 
+      React.createElement("div", {
+        id: "connect", 
+        style: style, 
+        className: "connect_container"}, 
+        React.createElement(CSSTransitionGroup, {
+          component: "div", 
+          transitionEnter: false, 
+          transitionName: "connect_init"}, 
+           !sent && !sending && !error ? formView : null
+        ), 
+        React.createElement(CSSTransitionGroup, {
+          component: "div", 
+          transitionName: "connect_sent"}, 
+           sent ? sentView : null
+        ), 
+        React.createElement(CSSTransitionGroup, {
+          component: "div", 
+          transitionName: "connect_sending"}, 
+           sending ? sendingView : null
+        ), 
+        React.createElement(CSSTransitionGroup, {
+          component: "div", 
+          transitionName: "connect_sending"}, 
+           error ? errorView : null
+        )
       )
     );
   }
--- components/connect/Connect.jsx
+++ components/connect/Connect.jsx
@@ -1,6 +1,7 @@
-var React = require('react'),
-    CSSTransitionGroup = React.addons.CSSTransitionGroup,
-    { StateStreamMixin } = require('rx-react'),
+var Rx = require('rx'),
+    React = require('react'),
+
+  { StateStreamMixin } = require('rx-react'),
 
     globular = require('../globular'),
     debug = require('debug')('r3dm:components:connect'),
@@ -8,18 +9,55 @@ var React = require('react'),
     ConnectActions = require('./Actions'),
     ConnectStore = require('./Store');
 
+var {
+    CSSTransitionGroup,
+    PureRenderMixin
+  } = React.addons;
+
+var Sent = React.createClass({
+
+  mixins: [PureRenderMixin],
+
+  render: function() {
+    return (
+      <div
+        style={ this.props.style }
+        className={ this.props.className }
+        key='sent'>
+        <h1>Thanks!</h1>
+        <p>You should see an email from us soon.</p>
+      </div>
+    );
+  }
+});
+
 var Connect = React.createClass({
 
   mixins: [StateStreamMixin],
 
-  _onEmailChange: ConnectActions.onEmailChange,
-  _onNameChange: ConnectActions.onNameChange,
-
   getStateStream: function() {
     debug('setting up state stream');
-    return ConnectStore;
+    return Rx.Observable.combineLatest(
+      ConnectStore,
+      this._compHeightAction,
+      function(state, height) {
+        state.height = height;
+        return state;
+      }
+    );
   },
 
+  componentDidMount: function() {
+    var connectForm = this.refs.form.getDOMNode();
+
+    this._compHeightAction.onNext(connectForm.clientHeight);
+  },
+
+  _onEmailChange: ConnectActions.onEmailChange,
+  _onNameChange: ConnectActions.onNameChange,
+  _compHeightAction: new Rx.BehaviorSubject(false),
+
+  // TODO: turn this to a single action
   _handleConnect: function(e) {
     var email = this.state.email,
         name = this.state.name;
@@ -46,71 +84,122 @@ var Connect = React.createClass({
       name,
       sending,
       sent,
-      error
+      error,
+      height
     } = this.state;
-    var view = [];
-    if (!sending && !error && !sent) {
-      view.push(
-        <div id = 'connect' className = 'connect' key='init'>
-          <div className = 'connect_heading'>
-            <h2>Work With Us.</h2>
-          </div>
-          <div className = 'connect_form'>
-            <div>
-              <form
-                action = ''
-                className = 'pure-form'
-                onSubmit = { this.handleConnect }>
-                <div className = 'connect_name'>
-                    <input
-                      type = 'text'
-                      name = 'name'
-                      className = 'connect_input'
-                      value = { name }
-                      onChange = { this._onNameChange }
-                      placeholder = 'your name' />
-                </div>
-                <div className = 'connect_email'>
-                  <div>
-                    <input
-                      type = 'email'
-                      name = 'email'
-                      className = 'connect_input'
-                      value = { email }
-                      onChange = { this._onEmailChange }
-                      placeholder = 'email'/>
-                  </div>
-                  <div
-                    className = 'button'
-                    onClick = { this._handleConnect }>
-                    <span>
-                      Connect
-                    </span>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
+
+    var style = {
+      height: height + 'px'
+    };
+
+    var sendingView = (
+      <div
+        ref='sending'
+        style={ style }
+        className='connect connect_sending'
+        key='sending'>
+        <svg
+          className="connect_sending-spinner"
+          width="65px"
+          height="65px"
+          viewBox="0 0 66 66"
+          xmlns="http://www.w3.org/2000/svg">
+          <circle
+            className="connect_sending-path"
+            fill="none"
+            strokeWidth="6"
+            strokeLinecap="round"
+            cx="33"
+            cy="33"
+            r="30">
+          </circle>
+        </svg>
+      </div>
+    );
+    var sentView = (
+      <Sent
+        ref='sent'
+        className='connect'
+        style={ style }/>
+    );
+    var errorView = (
+      <span
+        style={ style }
+        className='connect connect-error'
+        key='error'>Error</span>
+    );
+    var formView = (
+      <div
+        className='connect'
+        key='init'
+        ref='form'>
+        <div className='connect_heading'>
+          <h2>Work With Us.</h2>
         </div>
-      );
-    } else if (sending) {
-      view.push(
-        <div key='sending'></div>
-      );
-    } else if (sent) {
-      view.push(
-        <span key='sent'>Email sent</span>
-      );
-    } else {
-      view.push(
-        <span key='error'>Error</span>
-      );
-    }
+        <div className = 'connect_form'>
+          <form
+            action = ''
+            className = 'pure-form'
+            onSubmit = { this.handleConnect }>
+            <div className = 'connect_name'>
+                <input
+                  type = 'text'
+                  name = 'name'
+                  className = 'connect_input'
+                  value = { name }
+                  onChange = { this._onNameChange }
+                  placeholder = 'your name' />
+            </div>
+            <div className = 'connect_email'>
+              <div>
+                <input
+                  type = 'email'
+                  name = 'email'
+                  className = 'connect_input'
+                  value = { email }
+                  onChange = { this._onEmailChange }
+                  placeholder = 'email'/>
+              </div>
+              <div
+                className = 'button'
+                onClick = { this._handleConnect }>
+                <span>
+                  Connect
+                </span>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
 
     return (
-      <CSSTransitionGroup transitionName='connect'>
-        { view }
-      </CSSTransitionGroup>
+      <div
+        id='connect'
+        style={ style }
+        className='connect_container'>
+        <CSSTransitionGroup
+          component='div'
+          transitionEnter={ false }
+          transitionName='connect_init'>
+          { !sent && !sending && !error ? formView : null }
+        </CSSTransitionGroup>
+        <CSSTransitionGroup
+          component='div'
+          transitionName='connect_sent'>
+          { sent ? sentView : null }
+        </CSSTransitionGroup>
+        <CSSTransitionGroup
+          component='div'
+          transitionName='connect_sending'>
+          { sending ? sendingView : null }
+        </CSSTransitionGroup>
+        <CSSTransitionGroup
+          component='div'
+          transitionName='connect_sending'>
+          { error ? errorView : null }
+        </CSSTransitionGroup>
+      </div>
     );
   }
 });
