394e4a2e8de2548b6e54d7b94aafb9eb858156f9
Berkeley Martinez <Berkeley@robotie.com>
2015-01-29 12:26:39 -0800

    Add animations to state transitions

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var gulp = require('gulp'),
     // ## Bundle
     browserify = require('browserify'),
     watchify = require('watchify'),
-    envify = require('envify/custom')({ NODE_ENV: 'development' }),
+    envify = require('envify/custom'),
     uglifyify = require('uglifyify'),
     bundleName = require('vinyl-source-stream'),
     //brfs = require('brfs'),
@@ -187,7 +187,9 @@ function browserifyCommon(cb) {
   }
 
   var b = browserify(config);
-  b.transform(envify);
+  b.transform(envify({
+    NODE_ENV: 'development'
+  }));
   //b.transform(brfs);
 
   if (!production) {
