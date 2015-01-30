32cbc7e570f2af2ebf6403af1192b843af47296b
Harry Moreno <morenoh149@gmail.com>
2015-01-29 18:11:12 -0800

    added navbar to 404 page. Centered 404 error text

--- components/errors/404.js
+++ components/errors/404.js
@@ -1,13 +1,21 @@
-var React = require('react');
+var React = require('react'),
+    Nav = require('../nav');
 
 var FourOhFour = React.createClass({displayName: "FourOhFour",
   render: function() {
     return (
-      React.createElement("div", {className: "middle-box text-center animated fadeInDown"}, 
-        React.createElement("h1", null, "404"), 
-        React.createElement("h3", {className: "font-bold"}, "Page Not Found"), 
-        React.createElement("div", {className: "error-desc"}, 
-          "Sorry, this is not the page you are looking for."
+      React.createElement("div", {className: "error-container"}, 
+        React.createElement(Nav, null), 
+        React.createElement("div", {className: "pure-g error-layout"}, 
+          React.createElement("div", {className: "pure-u-1-3"}), 
+          React.createElement("div", {className: "pure-u-1-3"}, 
+            React.createElement("h1", null, "404"), 
+            React.createElement("h3", {className: "font-bold"}, "Page Not Found"), 
+            React.createElement("div", {className: "error-desc"}, 
+              "Sorry, this is not the page you are looking for."
+            )
+          ), 
+          React.createElement("div", {className: "pure-u-1-3"})
         )
       )
     );
--- components/errors/404.jsx
+++ components/errors/404.jsx
@@ -1,13 +1,21 @@
-var React = require('react');
+var React = require('react'),
+    Nav = require('../nav');
 
 var FourOhFour = React.createClass({
   render: function() {
     return (
-      <div className = 'middle-box text-center animated fadeInDown'>
-        <h1>404</h1>
-        <h3 className = 'font-bold'>Page Not Found</h3>
-        <div className = 'error-desc'>
-          Sorry, this is not the page you are looking for.
+      <div className='error-container'>
+        <Nav></Nav>
+        <div className = 'pure-g error-layout'>
+          <div className='pure-u-1-3'></div>
+          <div className='pure-u-1-3'>
+            <h1>404</h1>
+            <h3 className = 'font-bold'>Page Not Found</h3>
+            <div className = 'error-desc'>
+              Sorry, this is not the page you are looking for.
+            </div>
+          </div>
+          <div className='pure-u-1-3'></div>
         </div>
       </div>
     );
