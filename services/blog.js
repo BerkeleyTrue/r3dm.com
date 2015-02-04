19448d766e2cc8a2fda3ac134c39b691cf3bf36c
Harry Moreno <morenoh149@gmail.com>
2015-02-03 17:10:45 -0800

    refactored blog service to work with mongoose findById.

--- services/blog.js
+++ services/blog.js
@@ -1,6 +1,55 @@
 var keystone = require('keystone'),
     debug = require('debug')('r3dm:services:blog');
 
+module.exports = {
+  name: 'blogService',
+
+  /* reads blog posts from the database
+   * if the admin is logged in he sees published and unpublished posts
+   * else the user only sees published posts
+   * also
+   * if a slug is provided in the url, a single post is rendered
+   * else, a list of 5 blog posts are returned
+   */
+  read: function(req, resource, params, config, cb) {
+    var where, limit, skip,
+        User = keystone.list('User');
+    var slugP = params.slug && params.slug !== 'undefined';
+
+    debug('params', params);
+    debug('params.slug', params.slug);
+    debug('req.session', req.session);
+
+    if (req.session && req.session.userId) {
+      User.model.findById(req.session.userId, function (err, user) {
+        if (err) { return err; }
+
+        if (user) {
+          debug('signed in conditional');
+          if (slugP) {
+            where = { slug: params.slug };
+            limit = 1;
+            performPostsQuery(where, limit, skip, cb);
+          } else {
+            limit = 5;
+            performPostsQuery(where, limit, skip, cb);
+          }
+        } else {
+          debug('no user by that id found! are you spoofing?');
+        }
+      });
+    } else if (slugP) {
+      where = { slug: params.slug, publishedDate: { $exists: true }};
+      limit = 1;
+      performPostsQuery(where, limit, skip, cb);
+    } else {
+      where = { publishedDate: { $exists: true }};
+      limit = 5;
+      performPostsQuery(where, limit, skip, cb);
+    }
+  }
+};
+
 /*
  * performs a mongoDB query on the Post model
  * accepts a whereClause, limit size, and skip size
@@ -25,72 +74,4 @@ var performPostsQuery = function(whereClause, limit, skip, cb) {
         cb(err);
       }
     );
-};
-
-/* verifies the user is logged in
- */
-var verifySessionByUserId = function(userId) {
-  var User = keystone.list('Post');
-
-  User.model.findOne({ _id: userId }, function (err, user) {
-    if (err) { return cb(err); }
-
-    if (!user) {
-      debug('no user by that id found! are you spoofing?');
-      where = { publishedDate: { $exists: true }};
-      performPostsQuery(where, limit, skip, cb);
-    } else {
-      debug('mongodb found user by id!');
-      performPostsQuery(where, limit, skip, cb);
-    }
-  });
-}
-
-module.exports = {
-  name: 'blogService',
-
-  /* reads blog posts for the database
-   * if the user is logged in he sees published and unpublished posts
-   * else the user only sees published posts
-   * also
-   * if a slug is provided in the url a single post is rendered
-   */
-  read: function(req, resource, params, config, cb) {
-    var where, limit, skip,
-        slugP, signedIn;
-
-    debug('params', params);
-    debug('params.slug', params.slug);
-    debug('req.session', req.session);
-
-    if (req.session && req.session.userId) {
-      signedIn = verifySessionByUserId(req.session.userId);
-    } else {
-      signedIn = false;
-    }
-
-    slugP = params.slug && params.slug !== 'undefined';
-
-    if (signedIn) {
-      debug('signed in conditional');
-      if (slugP) {
-        where = { slug: params.slug };
-        limit = 1;
-        performPostsQuery(where, limit, skip, cb);
-      } else {
-        limit = 5;
-        performPostsQuery(where, limit, skip, cb);
-      }
-    } else if (slugP) {
-      debug('slugP in conditional');
-      where = { slug: params.slug, publishedDate: { $exists: true } };
-      limit = 1;
-      performPostsQuery(where, limit, skip, cb);
-    } else {
-      debug('else conditional');
-      where = { publishedDate: { $exists: true } };
-      limit = 5;
-      performPostsQuery(where, limit, skip, cb);
-    }
-  }
 };
