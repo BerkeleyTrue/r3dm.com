f9afd80e82763e49c4183ea00022fea6e71f0145
Berkeley Martinez <Berkeley@robotie.com>
2015-02-12 15:15:05 -0800

    Image stacks on top based on media query within react.

--- components/work/Work.js
+++ components/work/Work.js
@@ -8,8 +8,37 @@ var React = require('react/addons'),
     Spinner = require('../common/Spinner'),
     WorkCopy = require('./WorkCopy');
 
+var screenTrigger = '(max-width: 30em)';
+
 var Work = React.createClass({displayName: "Work",
   mixins: [PureRender],
+
+  getInitialState: function() {
+    return {
+      smallScreen: false
+    };
+  },
+
+  componentDidMount: function() {
+    var matchMedia = typeof window !== 'undefined' ? window.matchMedia : null;
+
+    this._mql = matchMedia(screenTrigger);
+    this._mql.addListener(this._updateScreen);
+    this._updateScreen();
+  },
+
+  componentWillUpdate: function() {
+    this._mql.removeListener(this._updateScreen);
+  },
+
+  _updateScreen: function() {
+    if (this._mql.matches === this.state.smallScreen) {
+      return;
+    }
+    this.setState({
+      smallScreen: this._mql.matches
+    });
+  },
 
   render: function() {
 
@@ -26,7 +55,7 @@ var Work = React.createClass({displayName: "Work",
         ), 
         React.createElement("div", {className: "work_content"}, 
           React.createElement("article", null, 
-            React.createElement(WorkCopy, {imgFirst: true }, 
+            React.createElement(WorkCopy, {imgFirst:  this.state.smallScreen}, 
               React.createElement("div", {className: "work_copy"}, 
                 React.createElement("header", null, 
                   React.createElement("h3", null, "SHPE"), 
