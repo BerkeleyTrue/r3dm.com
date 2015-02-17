c22b397b0f862d8630df17b03c87e8d9c2e67a0b
Harry Moreno <morenoh149@gmail.com>
2015-02-17 14:55:02 -0800

    added 'translation' field to Posts model. The relationship is one-to-one until keystone fixes one-to-many emptying array bug.

--- models/post.js
+++ models/post.js
@@ -1,5 +1,6 @@
 var keystone = require('keystone'),
-    Types = keystone.Field.Types;
+    Types = keystone.Field.Types,
+    debug = require('debug')('r3dm:models:post');
 
 var Post = new keystone.List('Post', {
   map: {
@@ -50,6 +51,10 @@ Post.add({
       wysiwyg: true,
       height: 400
     }
+  },
+  translation: {
+    type: Types.Relationship,
+    ref: 'Post'
   }
 });
 
@@ -63,7 +68,8 @@ Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
 Post.schema.pre('save', function(next) {
   var myPost = this,
       err;
-  console.log('myPost', myPost);
+
+  debug('saving Post:', myPost);
   if (myPost.state === 'published') {
     if (!myPost.publishedDate) {
       err = new Error('Cannot publish a post without a publishedDate.');
@@ -75,6 +81,17 @@ Post.schema.pre('save', function(next) {
     }
   }
   next();
+});
+
+Post.schema.post('save', function(post) {
+  debug('saved Post:', post);
+  Post.model.findByIdAndUpdate(post.translation,
+                               { $set: { translation: post.id }},
+                               function(err, translation) {
+    if (err) { throw err; }
+
+    debug('translation updated:', translation);
+  });
 });
 
 Post.register();
