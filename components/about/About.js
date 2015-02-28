b461ecacbd792c1ec5ce2327782182932290cf93
Harry Moreno <morenoh149@gmail.com>
2015-02-27 21:16:29 -0800

    Correct About copy typo

--- components/about/About.js
+++ components/about/About.js
@@ -11,10 +11,10 @@ var About = React.createClass({displayName: "About",
             React.createElement("h2", null, "About R3DM")
           ), 
           React.createElement("p", null, 
-            "We are a group of developers and designers who looking for exciting" + ' ' +
-            "new ideas to build and express our creativity. We build using" + ' ' +
-            "Node.js and the latest technologies to deliver a fast and beautiful" + ' ' +
-            "experience to users.", 
+            "We are a group of developers and designers who are looking for" + ' ' +
+            "exciting new ideas to build and express our creativity. We build" + ' ' +
+            "using Node.js and the latest technologies to deliver a fast and" + ' ' +
+            "beautiful experience to users.", 
             React.createElement("br", null), 
             React.createElement("br", null), 
             "Need an MVP? Need an API that can scale to enterprise level? Want" + ' ' +
