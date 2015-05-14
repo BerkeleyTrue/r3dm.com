8ed8ea5ed50bfd41f2bb81e2716d8787ff91e2ff
Harry Moreno <morenoh149@gmail.com>
2015-05-13 19:34:44 -0700

    Use Flexbox on team links
    * allows for greater future flexibility
    * refactor TeamBlock.jsx logic to use an object lookup

--- components/team/TeamBlock.jsx
+++ components/team/TeamBlock.jsx
@@ -3,44 +3,23 @@ var React = require('react');
 var Block = React.createClass({
   render: function() {
     var links = this.props.data.links.map(function(linkObj) {
-      var result = [];
-      if (linkObj.type === 'github') {
-        result.push((
-          <a href={ linkObj.url }>
-            <span className="fa-stack fa-lg">
-              <i className="fa fa-circle fa-stack-2x"></i>
-              <i className="fa fa-github-alt fa-stack-1x"></i>
-            </span>
-          </a>
-        ));
-      } else if (linkObj.type === 'portfolio') {
-        result.push((
-          <a href={ linkObj.url }>
-            <span className="fa-stack fa-lg">
-              <i className="fa fa-circle fa-stack-2x"></i>
-              <i className="fa fa-desktop fa-stack-1x"></i>
-            </span>
-          </a>
-        ));
-      } else if (linkObj.type === 'linkedin') {
-        result.push((
-          <a href={ linkObj.url }>
-            <span className="fa-stack fa-lg">
-              <i className="fa fa-circle fa-stack-2x"></i>
-              <i className="fa fa-linkedin fa-stack-1x"></i>
-            </span>
-          </a>
-        ));
-      } else if (linkObj.type === 'email') {
-        result.push((
-          <a href={ linkObj.url }>
-            <span className="fa-stack fa-lg">
-              <i className="fa fa-circle fa-stack-2x"></i>
-              <i className="fa fa-envelope fa-stack-1x"></i>
-            </span>
-          </a>
-        ));
+      // mapping of link-types to font-awesome icon names
+      var map = {
+        'github': 'github-alt',
+        'portfolio': 'desktop',
+        'linkedin': 'linkedin',
+        'email': 'envelope'
       }
+      var result = [];
+
+      result.push((
+        <a href={ linkObj.url }>
+          <span className="fa-stack fa-lg">
+            <i className="fa fa-circle fa-stack-2x"></i>
+            <i className={ "fa fa-stack-1x fa-" + map[linkObj.type] } ></i>
+          </span>
+        </a>
+      ));
       return result;
     });
 
@@ -49,7 +28,9 @@ var Block = React.createClass({
         <img src= { this.props.data.imgUrl } />
         <div><h3>{ this.props.data.name }</h3></div>
         <div><p>{ this.props.data.copy }</p></div>
-        { links }
+        <div className="links-container">
+          { links }
+        </div>
       </div>
     );
   }
