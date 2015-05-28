06257c68cde194e9f2d2014a56c36169f670f0c4
Berkeley Martinez <Berkeley@robotie.com>
2015-05-27 17:49:37 -0700

    Add react ink to hash links Router links are blown away when click so ink has no effect

--- components/nav/Links.jsx
+++ components/nav/Links.jsx
@@ -1,12 +1,14 @@
-var React = require('react'),
-    Router = require('react-router'),
-    classNames = require('classnames'),
-    Link = Router.Link;
+import React from 'react';
+import Ink from 'react-ink';
+import { Link } from 'react-router';
+import classNames from 'classnames';
 
-var Links = React.createClass({
-  displayName: 'Links',
-
-  propTypes: {
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  static displayName = 'Links';
+  static propTypes = {
     className: React.PropTypes.string,
     hash: React.PropTypes.string,
     links: React.PropTypes.arrayOf(
@@ -15,25 +17,24 @@ var Links = React.createClass({
         path: React.PropTypes.string
       })
     )
-  },
+  }
 
-  getInitialState: function() {
-    return {
-      hash: ''
-    };
-  },
+  render() {
+    const {
+      className,
+      hash
+    } = this.props;
 
-  render: function() {
-    var hash = this.props.hash;
-    var val = this.props.links.map(function(link) {
+    const val = this.props.links.map(function(link) {
       if (link.path.indexOf('#') !== -1) {
-        var underLineClass = classNames({
+        const underLineClass = classNames({
           'nav_underline': true,
           'active': hash && link.path.indexOf(hash) !== -1
         });
         return (
           <li key={ link.path }>
             <a href={ link.path } target='_self'>
+              <Ink />
               { link.name }
             </a>
             <div className={ underLineClass }></div>
@@ -42,17 +43,18 @@ var Links = React.createClass({
       } else {
         return (
           <li key={ link.path }>
-            <Link to={ link.path }>{ link.name }</Link>
+            <Link to={ link.path }>
+              { link.name }
+            </Link>
           </li>
         );
       }
     });
 
     return (
-      <ul className={ this.props.className }>
+      <ul className={ className }>
         { val }
       </ul>
     );
   }
-});
-module.exports = Links;
+}
