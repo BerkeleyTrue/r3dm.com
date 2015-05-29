a720dd4d08ff8cef752a6eec239cc9cac08bdced
Berkeley Martinez <Berkeley@robotie.com>
2015-05-29 15:20:46 -0700

    Fix date string rendering diffently in node/browser closes #62

--- components/util/index.js
+++ components/util/index.js
@@ -1,13 +1,28 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
 export { default as ScrollMixin } from './scrollMixin';
 
 export function formatDate(dateString) {
   if (!dateString) {
     return 'not published';
   }
-  return new Date(dateString)
-    .toLocaleString('en-US', {
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric'
-    });
+  const date = new Date(dateString);
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${ month } ${ day }, ${ year }`;
 }
